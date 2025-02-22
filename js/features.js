27edf61056f8419bfce1909d7be3d160e9bf0164
lijanahmmed <lijanahmmed@gmail.com>
2025-02-22 20:15:31 +0600

    full final payoo

--- js/features.js
+++ js/features.js
@@ -1,11 +1,20 @@
+const showCashOutBtn = document.getElementById('show-cash-out')
+const showAddMoneyBtn = document.getElementById('show-add-money')
+
 document.getElementById('show-cash-out').addEventListener('click', function(){
     const addMoneyForm = document.getElementById('add-money-form')
     const cashOutForm = document.getElementById('cash-out-form')
     addMoneyForm.classList.add('hidden')
     cashOutForm.classList.remove('hidden')
+    showCashOutBtn.classList.add('bg-green-400')
+    showAddMoneyBtn.classList.remove('bg-green-400')
+
+
 })
 
 document.getElementById('show-add-money').addEventListener('click', function(){
     document.getElementById('add-money-form').classList.remove('hidden')
     document.getElementById('cash-out-form').classList.add('hidden')
+    showCashOutBtn.classList.remove('bg-green-400')
+    showAddMoneyBtn.classList.add('bg-green-400')
 })
